b1c33d56e2564d8e162ddaed972981fe239c808e
viniciuspxc <viniciuspxc@gmail.com>
2024-03-07 00:43:43 -0300

    allauth setup

--- invest_planner/base/views.py
+++ invest_planner/base/views.py
@@ -6,43 +6,10 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 
-from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 
 from .models import Task
-
-
-class CustomLoginView(LoginView):
-    template_name = 'base/login.html'
-    fiels = '__all__'
-    redirect_authenticated_user = True
-    
-    def get_success_url(self):
-        return reverse_lazy('tasks')
-    
-class RegisterPage(FormView):
-    template_name = 'base/register.html'
-    form_class = UserCreationForm
-    redirect_authenticated_user = True
-    success_url = reverse_lazy('tasks')
-    
-    def form_valid(self, form):
-        user = form.save()
-        if user is not None:
-            login(self.request, user)
-        return super(RegisterPage, self).form_valid(form)
-    
-    def get(self, *args, **kwargs):
-        if self.request.user.is_authenticated:
-            return redirect('tasks')
-        return super(RegisterPage, self).get(*args, **kwargs)
-
-# Create your views here.
-# def taskList(request):
-#     return HttpResponse('Test view')
-
 
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
